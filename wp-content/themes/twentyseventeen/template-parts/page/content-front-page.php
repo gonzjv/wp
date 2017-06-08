408d34e9d67c082f420dd3a0ca914a4650d4e8f6
gonzjv <gonzjv@ya.ru>
2017-06-08 16:17:09 +0200

    work-08-06-17

--- wp-content/themes/twentyseventeen/template-parts/page/content-front-page.php
+++ wp-content/themes/twentyseventeen/template-parts/page/content-front-page.php
@@ -30,12 +30,12 @@
 
 	<div class="panel-content">
 		<div class="wrap">
-			<header class="entry-header">
-				<?php the_title( '<h2 class="entry-title">', '</h2>' ); ?>
+<!--			<header class="entry-header">
+				<?php // the_title( '<h2 class="entry-title">', '</h2>' ); ?>
 
-				<?php twentyseventeen_edit_link( get_the_ID() ); ?>
+				<?php // twentyseventeen_edit_link( get_the_ID() ); ?>
 
-			</header><!-- .entry-header -->
+			</header> .entry-header -->
 
 			<div class="entry-content">
 				<?php
